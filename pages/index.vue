88d1c2498a3c639629802ff31cf6952d5dfcd6d5
DieuDo <d.do@shopware.com>
2025-03-17 19:35:37 +0700

    Update img

--- pages/index.vue
+++ pages/index.vue
@@ -9,17 +9,19 @@ useHead({
       href: "https://fonts.googleapis.com/css2?family=Marmelad&family=Oooh+Baby&family=OpenSans&display=swap",
     },
   ],
-  title: "DIỆU ♡ HÂN-23/03/2025",
+  title: "DIỆU♡HÂN - 23/03/2025",
   meta: [
-    { property: "og:title", content: "DIỆU ♡ HÂN-23/03/2025" },
+    { property: "og:title", content: "DIỆU♡HÂN - 23/03/2025" },
     { property: "og:description", content: "Hãy cùng chờ đón ngày trọng đại của chúng tôi vào 23/03/2025!" },
     { property: "og:image", content: "https://dieuhanwedding.github.io/images/slide/001.webp" },
+    { property: "og:image:width", content: "1200" },
+    { property: "og:image:height", content: "630" },
     { property: "og:url", content: "https://dieuhanwedding.github.io/" },
     { property: "og:type", content: "website" },
     { name: "twitter:card", content: "summary_large_image" },
-    { name: "twitter:title", content: "DIỆU ♡ HÂN-23/03/2025" },
+    { name: "twitter:title", content: "DIỆU♡HÂN - 23/03/2025" },
     { name: "twitter:image", content: "https://dieuhanwedding.github.io/images/slide/001.webp" },
-  ]
+  ],
 });
 </script>
 <template>
